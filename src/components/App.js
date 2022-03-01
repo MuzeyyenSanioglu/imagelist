36099cc1047ad399302f8c57332eaa75e2394ba6
Müzeyyen SANİOĞLU <56695234+MuzeyyenSanioglu@users.noreply.github.com>
2022-03-01 15:50:57 +0300

    Search button

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import SearchInput from "./SearchInput";
+
+const App = () =>{
+  return (
+    <div className="ui container" style={{marginTop : '30px'}}>
+      <SearchInput />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/SearchInput.js
@@ -0,0 +1,25 @@
+import React  from "react";
+
+class SearchInput extends React.Component{
+
+    onInputChange(event){
+        console.log(event.target.value)
+    }
+    render(){
+        return (
+            <div className="ui segment">
+                <form className="ui form" action="">
+                    <div className="field">
+                        <div className="ui massice icon input">
+                        <input type="text" placeholder="Search" onChange={this.onInputChange}/>
+                        <i className="search icon"></i>
+                        </div>
+                        </div>                   
+                         
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SearchInput;
