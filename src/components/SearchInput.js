55008f9099c3e80905c9d67145751772c3a841bd
Müzeyyen SANİOĞLU <56695234+MuzeyyenSanioglu@users.noreply.github.com>
2022-03-02 11:45:05 +0300

    Connection with components

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -8,7 +8,7 @@ class SearchInput extends React.Component{
     }
     onFormSubmit = (event) =>{
         event.preventDefault();
-        console.log(this.state.entry)
+        this.props.onSearchSubmit(this.state.entry);
     }
     render(){
         return (
