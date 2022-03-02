4d38cbd40608fb5a11205cd4cc8094df1add1de3
Müzeyyen SANİOĞLU <56695234+MuzeyyenSanioglu@users.noreply.github.com>
2022-03-02 15:59:50 +0300

    Apı Call

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,28 @@
 import React from "react";
+import axios from "axios";
 import SearchInput from "./SearchInput";
+import ImageList from "./ImageList";
 
 class App extends React.Component {
-  onSearchSubmit = (entry) => {
-    console.log(entry);
+  constructor(props){
+    super(props);
+    this.state = { images : []};
+
+
+  }
+  onSearchSubmit =  async (entry) => {
+    const response =  await axios.get(`https://pixabay.com/api/?key=25949967-7868a9de315b3d5b8d30320ef&q=${entry}&image_type=photo`)
+
+    this.setState({images : response.data.hits});
+
   }
   render(){
     return (
       <div className="ui container" style={{marginTop : '30px'}}>
         <SearchInput onSearchSubmit= {this.onSearchSubmit} />
+        <ImageList images={this.state.images} />
       </div>
-    )
+    );
 
   }
 }
